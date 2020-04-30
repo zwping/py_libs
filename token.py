48faf3e3664a75d7671f3994f8bc8ccf424f90ab
zwping <1101558280@qq.com>
2020-04-30 15:06:52 +0800

    '1'

--- token.py
+++ token.py
@@ -4,14 +4,9 @@ import functools
 from flask import request
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
-from libs.log import i
+from config import app
+from libs.empty_util import isEmpty
 from libs.response_standard import response
-from libs.db import DB
-from libs.empty_util import isEmpty, isNotEmpty
-
-# from spider.service_log import service_log
-
-SECRET_KEY = 'zwping'
 
 
 def ctoken(data, expires=604800):
@@ -21,7 +16,7 @@ def ctoken(data, expires=604800):
      """
     # 第一个参数是内部的私钥，不可外泄
     # 第二个参数是有效期(秒)
-    s = Serializer(SECRET_KEY, expires_in=expires)
+    s = Serializer(app.config['SECRET_KEY'], expires_in=expires)
     # 接收用户id转换与编码
     token = s.dumps({"data": data}).decode("ascii")
     return token
@@ -31,7 +26,7 @@ def vtoken(token):
     """ 校验token """
 
     # 参数为私有秘钥，跟上面方法的秘钥保持一致
-    s = Serializer(SECRET_KEY)
+    s = Serializer(app.config['SECRET_KEY'])
     try:
         # 转换为字典
         data = s.loads(token)

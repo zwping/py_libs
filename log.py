e3c3082f7f7e84a9733dce65cd134c7d4b34e9e0
zwping <1101558280@qq.com>
2020-05-09 00:31:54 +0800

    '1'

--- log.py
+++ log.py
@@ -4,5 +4,5 @@ from config import app
 def i(log):
     # print('-------------- log i START---------------')
     if app.config['LOG']:
-        print('============== %s ==============' % log)
+        print('<br />============== %s ==============' % log)
     # print('-------------- log i END  ---------------')
